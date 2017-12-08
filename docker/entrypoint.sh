2d83b9321b3c6d9a4119369d0a9330eba26311c5
Markus Blaschke <mblaschke82@gmail.com>
2017-12-08 08:24:15 +0100

    Implement env settings

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,9 @@
 
 CONF_PATH="/app/src/k8s-devops-console/conf/app.conf"
 
-if [ -n "${OAUTH_PROVIDER+x}" ]; then
-    echo "oauth.provider = ${OAUTH_PROVIDER}" >> "$CONF_PATH"
-fi
-
-if [ -n "${OAUTH_CLIENT_ID+x}" ]; then
-    echo "oauth.client.id = ${OAUTH_CLIENT_ID}" >> "$CONF_PATH"
-fi
-
-if [ -n "${OAUTH_CLIENT_SECRET+x}" ]; then
-    echo "oauth.client.secret = ${OAUTH_CLIENT_SECRET}" >> "$CONF_PATH"
+if [ -z "${APP_SECRET+x}" ]; then
+    echo "Auto generating APP_SECRET"
+    export APP_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 fi
 
 exec "$@"
