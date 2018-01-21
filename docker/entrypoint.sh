628dd0452db2f10ebb885a9e593da35cb0cfafe1
Markus Blaschke <mblaschke82@gmail.com>
2018-01-21 18:08:25 +0100

    Create warning if APP_SECRET is empty

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 CONF_PATH="/app/src/k8s-devops-console/conf/app.conf"
 
 if [ -z "${APP_SECRET+x}" ]; then
-    echo "Auto generating APP_SECRET"
+    echo "[WARN] environment var APP_SECRET is empty, generating it"
     export APP_SECRET=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 fi
 
